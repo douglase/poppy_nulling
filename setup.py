38b6ab5291bc52120e9088403bd8f017255aba96
douglase <douglase@bu.edu>
2014-10-20 22:09:15 -0400

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='poppy-nulling',
+      version='0.2',
+      description='Nulling with Physical Optics Propagation for Python',
+      author='E.S. Douglas',
+      url='http://www.github.com/douglase/poppy-nulling',
+      py_modules=['null', 'nulling_utils'],
+     )
